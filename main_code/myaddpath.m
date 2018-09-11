0ec704f30fbea72c39ef45991dd169085dbe7888
sidgairo18 <siddhartha.gairola18@gmail.com>
2018-09-11 16:06:23 +0530

    Added slightly modified code

---
+++ main_code/myaddpath.m
@@ -0,0 +1,18 @@
+% Author: Carl Doersch (cdoersch at cs dot cmu dot edu)
+%
+% set up the matlab path if it hasn't been done yet.
+function myaddpath(force)
+%there's really no easier way to check if this function has been called?
+%if(nargin>0||numel(strfind(path,'crossValClustering'))==0)
+  addpath('dswork');
+  addpath('crossValClustering');
+  addpath('GSwDownloader');
+  addpath('html');
+  addpath('hog');
+  addpath('MinMaxSelection');
+  libHome='/home/sid/cv-libs/';
+  if(strcmp(libHome,'PATH_TO_LIBSVM'))
+    error('set libsvm path in myaddpath.m!!');
+  end
+  addpath([libHome 'libsvm-3.23/']);%3.0-1 is the release we used
+%end
